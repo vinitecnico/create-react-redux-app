ac89e27f5b130dd44e86a2ed537641a1dc72b7cf
vinicius rodrigues <vinitecnico@gmail.com>
2019-10-24 21:01:31 -0300

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Provider />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
